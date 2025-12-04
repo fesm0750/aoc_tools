da94c0da0594eb4f5541c68f1aaa8aa69f2e9e33
Fernando Martins <emmanuel.martins@outlook.com>
2025-12-04 15:43:18 -0300

    feat(Grid): add new fns to create Grids - `from_iter`: creates a new grid from an iterator; - `with_borders`: creates a new grid from an iterator and add borders to it.

--- src/grid.rs
+++ src/grid.rs
@@ -71,6 +71,53 @@ impl<T: Clone> Grid<T> {
 
         Grid { flat: v, len_x, len_y }
     }
+
+    /// The input iterator `iter` must have at least as many elements as `len_x` *
+    /// `len_y`, otherwise the function panics. If it has more elements, the remaining are
+    /// ignored.
+    ///
+    /// # Panics
+    ///
+    /// - If `iter` does not have enough elements to fill the grid.
+    pub fn from_iter<I>(len_x: usize, len_y: usize, iter: I) -> Grid<T>
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let size = len_x * len_y;
+        let flat = iter.into_iter().take(size).collect::<Vec<T>>();
+
+        debug_assert!(flat.len() == size);
+        Grid::from_vec(len_x, len_y, flat)
+    }
+
+    /// Creates a new grid from a iterator and adds a border to it. Also receives as
+    /// parameters, the neutral element to be used as a `border` and the border
+    /// `thickness` as rows or columns.
+    ///
+    /// Iterator must be finite, if it does not have enough items to complete the last
+    /// line, the remaining elements will be completed with the border value.
+    ///
+    /// # Assumes
+    ///
+    /// - Iterator is not infinite.
+    pub fn with_borders<I>(len_x: usize, border: T, thickness: usize, iter: I) -> Grid<T>
+    where
+        I: IntoIterator<Item = T>,
+        T: Copy,
+    {
+        let mut iter = iter.into_iter().peekable();
+        let mut flat = Vec::<T>::new();
+        let len = len_x + 2;
+        flat.extend(vec![border; len * thickness]); // upper border
+        while iter.peek().is_some() {
+            (0..thickness).for_each(|_| flat.push(border)); // left border
+            flat.extend(iter.by_ref().take(len_x));
+            (0..thickness).for_each(|_| flat.push(border)); // right border
+        }
+        flat.extend(vec![border; (1 + thickness) * len - flat.len() % len]); // complete last line and add lower border
+
+        Grid::from_vec(len, flat.len() / len, flat)
+    }
 }
 
 impl<T> Grid<T> {
